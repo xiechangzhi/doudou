b4dcbd8bc6494969091c7ae8915913e426c05c17
谢长志 <139883181@qq.com>
2019-11-27 11:06:59 +0800

    doudou-manage后台管理模块实现三级目录查询平台属性改

--- doudou-api/src/main/java/com/xcz/doudou/service/PmsBaseAttrService.java
+++ doudou-api/src/main/java/com/xcz/doudou/service/PmsBaseAttrService.java
@@ -4,11 +4,12 @@ import com.xcz.doudou.pojo.PmsBaseAttrInfo;
 
 import java.util.List;
 
-public interface PmsBaseAttrInfoService {
+public interface PmsBaseAttrService {
     /**
      * 根据三级目录查询平台属性
      * @param catalog3Id
      * @return
      */
     List<PmsBaseAttrInfo> attrInfoList(String catalog3Id);
+
 }
--- doudou-manage-service/src/main/java/com/xcz/doudou/manage/service/impl/PmsBaseAttrServiceImpl.java
+++ doudou-manage-service/src/main/java/com/xcz/doudou/manage/service/impl/PmsBaseAttrServiceImpl.java
@@ -2,17 +2,21 @@ package com.xcz.doudou.manage.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.xcz.doudou.manage.mapper.PmsBaseAttrInfoMapper;
+import com.xcz.doudou.manage.mapper.PmsBaseAttrValueMapper;
 import com.xcz.doudou.pojo.PmsBaseAttrInfo;
-import com.xcz.doudou.service.PmsBaseAttrInfoService;
+import com.xcz.doudou.service.PmsBaseAttrService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @Service
-public class PmsBaseAttrInfoServiceImpl implements PmsBaseAttrInfoService {
+public class PmsBaseAttrServiceImpl implements PmsBaseAttrService {
 
     @Autowired
     PmsBaseAttrInfoMapper pmsBaseAttrInfoMapper;
+
+    @Autowired
+    PmsBaseAttrValueMapper pmsBaseAttrValueMapper;
 
     @Override
     public List<PmsBaseAttrInfo> attrInfoList(String catalog3Id) {
@@ -21,5 +25,6 @@ public class PmsBaseAttrInfoServiceImpl implements PmsBaseAttrInfoService {
         pmsBaseAttrInfo.setCatalog3Id(catalog3Id);
         List<PmsBaseAttrInfo> pmsBaseAttrInfos = pmsBaseAttrInfoMapper.select(pmsBaseAttrInfo);
         return pmsBaseAttrInfos;
+
     }
 }
